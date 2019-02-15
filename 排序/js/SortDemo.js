e4dc5deb9916b4f5c07f99bde4f9cdd8521ea72f
懒点 <784178252@qq.com>
2019-02-15 22:29:49 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

--- 排序/js/SortDemo.js
+++ 排序/js/SortDemo.js
@@ -4,14 +4,15 @@ var arr = [2,238,35,75,32];
 function bubbleSort(arr) {
     var len = arr.length;
     for (var i = 0; i < len - 1; i++) {
-        for (var j = i + 1; j < len - 1; j++) {
-            if (arr[i] > a[j]) {
-                var temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+        for (var j = i + 1; j < len; j++) {
+            if (arr[i] > arr[j]) {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
+    return arr;
 };
 
 console.log(bubbleSort(arr))
